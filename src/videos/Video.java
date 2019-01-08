94a335b3fb5f0b10e94b45ec26ee17b264137fbe
Alejandro Calle Prado <alex_calle@hotmail.com>
2019-01-08 16:21:16 +0100

    Initial code for Video class. Code for setting URL address not implemented.

--- src/videos/Video.java
+++ src/videos/Video.java
@@ -1,5 +1,32 @@
 package videos;
 
+import java.util.*;
+
 public class Video {
+	private String urlAddress;
+	private String title;
+	private List<String> tags;
+	
+	public Video(String title, List<String> tags) {
+		this.title = title;
+		this.tags = tags;
+		//falta crear la URL del video
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getURL() {
+		return urlAddress;
+	}
+	public List<String> getTags(){
+		return tags;
+	}
+	public void modifyTitle(String newTitle) {
+		title = newTitle;
+	}
+	public void modifyTags(List<String> newTags) {
+		tags = newTags;
+	}
 
 }
