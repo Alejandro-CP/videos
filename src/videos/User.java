0dc1c9a57653e3566976cbe50ef8aa422415ed56
Alejandro Calle Prado <alex_calle@hotmail.com>
2019-01-08 16:20:40 +0100

    Inital code for User class. Creation of videos no implemented.

--- src/videos/User.java
+++ src/videos/User.java
@@ -1,5 +1,32 @@
 package videos;
 
-public class User {
+import java.util.*;
 
+public class User {
+	private String name;
+	private String surname;
+	private String password;
+	private Date registrationDate;
+	
+	public User(String name, String surname, String password) {
+		this.name = name;
+		this.surname = surname;
+		this.password = password;
+		registrationDate = new Date();
+	}
+	
+	public String getUserName() {
+		return name + "." + surname;
+	}
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+	public void modifyPassword(String password, String newPassword) {
+		if(this.password.equals(password)) {
+			this.password = newPassword;
+			System.out.println("Contrassenya canviada correctament.");
+		}else {
+			System.out.println("Contrassenya incorrecta.\n" + "No s'ha canviat la contrassenya.");
+		}
+	}
 }
