e48babdbada0b96163d6f89c0fcd59f8cda16ea9
Alejandro Calle Prado <alex_calle@hotmail.com>
2019-01-10 11:01:33 +0100

    added video creation from user added test2 function for testing video creation

--- src/videos/Main.java
+++ src/videos/Main.java
@@ -1,27 +1,27 @@
 package videos;
 
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 public class Main {
 	static Map<String, User> userList = new HashMap<>();
 	static Map<Integer, Video> videoList = new HashMap<>();
+	static List<String> genericTagList = new ArrayList<>(Arrays.asList("Animals", "Gatets", "Mamífers", "Gats"));
 
 	public static void main(String[] args) {
-		test1();
+		//test1();
+		test2();
 		}
 	
 	public static void addUser(String name, String surname, String password) {
 		if(checkUserName(name, surname) && checkPassword(password)) {
-			String username = name + "." + surname;
-			username = username.toLowerCase();
+			String username = getUsername(name, surname);
 			if(userList.containsKey(username)) {
 				System.out.println("L'usuari " + name + " " + surname + " ja està registrat.\nL'usuari no s'ha creat.\n");
 			}else {
 				userList.put(username, new User(name, surname, password));
 				System.out.println("L'usuari " + name + " " + surname +" s'ha creat correctament.\n");
-			}
-			
+			}		
 		}else {
 			System.out.println("L'usuari no s'ha creat.\n");
 		}
@@ -48,8 +48,31 @@ public class Main {
 		}
 	}
 	
+	public static boolean correctPassword(String name, String surname, String password) {
+		String username = getUsername(name, surname);
+		if(userList.get(username).checkPassword(password)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public static String getUsername(String name, String surname) {
+		String username = name + "." + surname;
+		return username.toLowerCase();
+	}
+	
+	public static void addVideo(String name, String surname, String password, String title, List<String> tags) {
+		if(correctPassword(name, surname, password)) {
+			userList.get(getUsername(name, surname)).createVideo(title, tags, videoList);
+			System.out.println("El vídeo s'ha creat correctament.\n");
+		}else {
+			System.out.println("Contrassenya incorrecta. El vídeo no s'ha creat.\n");
+		}
+	}
+	
 	// Testing creating and adding users
-	public static void test1(){
+	/*public static void test1(){
 		try {
 			addUser("", "López", "123456");
 			TimeUnit.SECONDS.sleep(2);
@@ -69,6 +92,12 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}*/
+	// Testing creation and addition of videos
+	public static void test2() {
+		addUser("Marco", "López", "1234567890");
+		addVideo("Marco", "López", "1234567890", "Video1", genericTagList);
+		addVideo("Marco", "López", "1234576890", "Video1", genericTagList);
 	}
 
 }
