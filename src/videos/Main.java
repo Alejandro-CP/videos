088911ec8197fd7bdc1ccd15a0a59798846ee47c
Alejandro Calle Prado <alex_calle@hotmail.com>
2019-01-15 12:22:35 +0100

    Testing new User and Video classes and methods. Everything works fine

--- src/videos/Main.java
+++ src/videos/Main.java
@@ -1,122 +1,31 @@
 package videos;
 
 import java.util.*;
-//import java.util.concurrent.TimeUnit;
 
 public class Main {
-	static Map<String, User> userList = new HashMap<>();
-	static Map<Integer, Video> videoList = new HashMap<>();
+	static List<Video> videoList = new ArrayList<>();
 	static List<String> genericTagList = new ArrayList<>(Arrays.asList("Animals", "Gatets", "Mamífers", "Gats"));
+	//static List<String> nullTagList = new ArrayList<>();
 
-	public static void main(String[] args) {
-		//test1();
-		//test2();
-		test3();
-		}
-	
-	public static void addUser(String name, String surname, String password) {
-		if(checkUserName(name, surname) && checkPassword(password)) {
-			if(userList.containsKey(getUsername(name, surname))) {
-				System.out.println("L'usuari/a " + name + " " + surname + " ja està registrat.\nL'usuari/a no s'ha creat.\n");
-			}else {
-				userList.put(getUsername(name, surname), new User(name, surname, password));
-				System.out.println("L'usuari/a " + name + " " + surname +" s'ha creat correctament.\n");
-			}		
-		}else {
-			System.out.println("L'usuari/a no s'ha creat.\n");
-		}
-	}
-	
-	public static boolean checkUserName(String name, String surname) {
-		if ((name.length() < 1) || (surname.length() < 1 )) {
-			System.out.println("El nom i el cognom han de tenir un caràcter com a mínim.");
-			return false;
-		}else {
-			return true;
-		}
-	}
-	
-	public static boolean checkPassword(String password) {
-		int lowLimit = 8;
-		int highLimit = 16;
-		if((password.length() < lowLimit) || (password.length() > highLimit)) {
-			System.out.println("La contrassenya ha de tenir un mínim de " + lowLimit + 
-					" caràcters i un màxim de " + highLimit + " caràcters.");
-			return false;
-		}else {
-			return true;
-		}
-	}
-	
-	public static boolean correctPassword(String name, String surname, String password) {
-		if(userList.get(getUsername(name, surname)).checkPassword(password)) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	public static String getUsername(String name, String surname) {
-		String username = name + "." + surname;
-		return username.toLowerCase();
-	}
-	
-	public static void addVideo(String name, String surname, String password, String title, List<String> tags) {
-		if(correctPassword(name, surname, password)) {
-			userList.get(getUsername(name, surname)).createVideo(title, tags, videoList);
-			System.out.println("El vídeo s'ha creat correctament.\n");
-		}else {
-			System.out.println("Contrassenya incorrecta. El vídeo no s'ha creat.\n");
-		}
-	}
-	
-	public static void listUserVideos(String name, String surname, String password) {
-		if(correctPassword(name, surname, password)) {
-			userList.get(getUsername(name, surname)).myVideos(videoList);
-		}
-	}
-	
-	// Testing creating and adding users
-	/*public static void test1(){
-		try {
-			addUser("", "López", "123456");
-			TimeUnit.SECONDS.sleep(2);
-			addUser("Marco", "", "1234567890");
-			TimeUnit.SECONDS.sleep(2);
-			addUser("Marco", "López", "1234567890123456789");
-			TimeUnit.SECONDS.sleep(2);
-			addUser("Marco", "López", "1234567890");
-			TimeUnit.SECONDS.sleep(2);
-			addUser("Marco", "López", "0987654321");
-			TimeUnit.SECONDS.sleep(2);
-			addUser("María", "Pérez", "0987654321");
-			for(Map.Entry<String, User> username : userList.entrySet()) {
-				username.getValue().getUserInfo();
-			}
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}*/
-	// Testing creation and addition of videos
-	/*public static void test2() {
-		addUser("Marco", "López", "1234567890");
-		addVideo("Marco", "López", "1234567890", "Video1", genericTagList);
-		addVideo("Marco", "López", "1234576890", "Video1", genericTagList);
-		addVideo("Marco", "López", "1234567890", "Video2", genericTagList);
-	}*/
-	// Testing listing user videos
-	public static void test3() {
-		addUser("Marco", "López", "1234567890");
-		addUser("María", "Pérez", "0987654321");
-		addVideo("Marco", "López", "1234567890", "Video1", genericTagList);
-		addVideo("Marco", "López", "1234567890", "Video2", genericTagList);
-		addVideo("Marco", "López", "1234567890", "Video3", genericTagList);
-		addVideo("Marco", "López", "1234567890", "Video4", genericTagList);
-		addVideo("María", "Pérez", "0987654321", "Video5", genericTagList);
-		addVideo("María", "Pérez", "0987654321", "Video6", genericTagList);
-		listUserVideos("Marco", "López", "1234567890");
-		listUserVideos("María", "Pérez", "0987654321");
+	public static void main(String[] args) throws Exception {
+		User user1 = new User("Juan", "Moreno", "0123456789");
+		User user2 = new User("Pedro", "López", "1234567890");
+		User user3 = new User("Monica", "Moreno", "9876543210");
+		//System.out.println(user1.getName() + " " + user1.getSurame() + ". ID:  " + user1.getUserId() + "\nData registre: " + user1.getRegistrationDate());
+		//System.out.println(user1.modPassword("0123456789", "1234567890"));
+		user1.createVideo("Vídeo 1.1", genericTagList, videoList);
+		user1.createVideo("Vídeo 1.2", genericTagList, videoList);
+		user1.createVideo("Vídeo 1.3", genericTagList, videoList);
+		user2.createVideo("Vídeo 2.1", genericTagList, videoList);
+		user2.createVideo("Vídeo 2.2", genericTagList, videoList);
+		user3.createVideo("Vídeo 3.1", genericTagList, videoList);
+		/*int i = 2;
+		System.out.println(videoList.get(i).getTitle() + "\n" + videoList.get(i).getCreator() + "\n" 
+		+ videoList.get(i).getVideoId() + "\n" + videoList.get(i).getURL() + "\n" + videoList.get(i).getTags());*/
+		System.out.println(user1.myVideos(videoList));
+		System.out.println(user2.myVideos(videoList));
+		System.out.println(user3.myVideos(videoList));
+
 	}
 
 }
